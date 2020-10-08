7c9f3e371fd45046fafd7df59c3bfeb98cb39c3f
Jhonnatan Eduardo Zamuidio Palacios <tecsco2013@gmail.com>
2020-10-08 09:29:57 -0500

    Se implemento la visualización solo de forma vertical y se agregaron las fuentes tipograficas, backup de seguridad en los archivos de preferencia y se definio la versión minima en SDK es API 16

--- app/src/main/java/com/jhonnatan/ajedrez/MainActivity.java
+++ app/src/main/java/com/jhonnatan/ajedrez/MainActivity.java
@@ -3,12 +3,19 @@ package com.jhonnatan.ajedrez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaración de los elementos
+    Button comenzar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Desactivamos el boton de atras
+    @Override public void onBackPressed() { }
 }
